7f36b6bc3e9ee50d2ae2ab6432b6307c711140c9
Anish Reddy <anishreddy202@gmail.com>
2015-12-17 14:42:14 -0600

    device info updates

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,11 +10,77 @@
   /** @ngInject */
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
+    vm.deviceInfo = {};
+    vm.onDrop = false;
+    vm.selectedCell = null;
+    vm.save = save;
+    vm.cancel =cancel;
+    vm.deviceType = null;
+    vm.showDeviceInfo = showDeviceInfo;
+    vm.showdelete = false;
+    vm.deleteDevice = deleteDevice;
 
-    vm.drop = drop;
-    vm.drag = drag;
 
+    function deleteDevice(){
+
+      if(vm.deviceType === 'camera'){
+        vm.selectedCell.camera.enable = false;
+        vm.selectedCell.camera.name = null;
+        vm.selectedCell.camera.url = null;
+      }
+      if(vm.deviceType === 'sensor'){
+        vm.selectedCell.sensor.enable = false;
+        vm.selectedCell.sensor.name = null;
+        vm.selectedCell.sensor.url = null;
+
+      }
+      vm.selectedCell = null;
+      vm.deviceInfo = {};
+      vm.onDrop = false;
+      vm.deviceType = null;
+    }
+    function showDeviceInfo(col,type){
+      vm.selectedCell = col;
+      vm.deviceType = type;
+      vm.onDrop = true;
+      vm.showdelete = true;
+
+      console.log(col);
+
+      if(vm.deviceType === 'camera'){
+        vm.deviceInfo.name = col.camera.name;
+        vm.deviceInfo.url = col.camera.url;
+      }
+      if(vm.deviceType === 'sensor'){
+        vm.deviceInfo.name = col.sensor.name;
+        vm.deviceInfo.url = col.sensor.url;
+      }
+
+      console.log(vm.deviceType)
+    }
+
+    function save(){
+      if(vm.deviceType === 'camera'){
+        vm.selectedCell.camera.enable = true;
+        vm.selectedCell.camera.name =  vm.deviceInfo.name;
+        vm.selectedCell.camera.url =  vm.deviceInfo.url;
+      }
+      if(vm.deviceType === 'sensor'){
+        vm.selectedCell.sensor.enable = true;
+        vm.selectedCell.sensor.name =  vm.deviceInfo.name;
+        vm.selectedCell.sensor.url =  vm.deviceInfo.url;
+      }
+      vm.deviceInfo = {};
+      vm.onDrop = false;  
+    }
+    function cancel(){
+      vm.selectedCell = null;
+      vm.deviceInfo = {};
+      vm.onDrop = false;
+      vm.deviceType = null;
+    }
     var x = new Array(6);
+
 
     for (var i = 0; i < 6; i++) {
       x[i] = buildArray(i)
@@ -33,7 +99,7 @@
 
       var arr = [];
       for(var i =0;i< 6;i++){
-        var  newObj = { camera: false,sensor:false,parking:false,droppable: false, column: i, row : row};
+        var  newObj = { camera: { enable:false},sensor:{enable:false},parking:false,droppable: false, column: i, row : row};
         if(obj[row].col.indexOf(i) >= 0){
           newObj.parking = true;
         }
@@ -45,29 +111,9 @@
       return arr
     }
 
-
-
-    // for(var i =0;i< x.length;i++){
-    //   console.log(x[i].length);
-    // }
-
-    console.log(x);
-    
-    //vm.grid[8][8];
     vm.grid = x
 
-
-
     vm.cameras = [];
-
-    function drop(i,j){
-      console.log(i)
-      console.log(j)
-    }
-
-    function drag(item){
-      console.log(item)
-    }
   }
 
   function draggable(){
@@ -91,15 +137,24 @@
       scope:{
         tableCell: '='
       },
-      link:function(scope,element,attrs){
+      link:function(scope,element,attrs,$rootScope){
         element.droppable({
           drop:function(event,ui){
-            if(event.toElement.title =='camera'){
-              scope.tableCell.camera = true
-            }
-            if(event.toElement.title =='sensor'){
-              scope.tableCell.sensor = true
-            }
+
+            //console.log($rootScope);
+            
+            scope.$parent.$parent.$parent.$parent.vm.selectedCell = scope.tableCell;
+            scope.$parent.$parent.$parent.$parent.vm.deviceType = event.toElement.title;
+            scope.$parent.$parent.$parent.$parent.vm.onDrop = true;
+            console.log(scope.$parent.$parent.$parent.$parent.vm);
+              //  scope.$parent.$parent.$parent.onDrop = true;
+             //scope.$apply();
+            // if(event.toElement.title =='camera'){
+            //   scope.tableCell.camera.enable = true
+            // }
+            // if(event.toElement.title =='sensor'){
+            //   scope.tableCell.sensor.enable = true
+            // }
             scope.$apply();
           }
         })
